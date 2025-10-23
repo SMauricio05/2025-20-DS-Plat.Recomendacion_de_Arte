1354c54b9d9524f17cb791edc84b29d4ef2bd2b0
SMauricio05 <72190212@continental.edu.pe>
2025-10-23 18:09:31 -0500

    Change NVARCHAR to VARCHAR in ObrasArte table

--- BD/SCRIPTS/Creación/tablaObrasArte.sql
+++ BD/SCRIPTS/Creación/tablaObrasArte.sql
@@ -1,10 +1,11 @@
 CREATE TABLE ObrasArte (
     id_obra INT IDENTITY(1,1) PRIMARY KEY,
-    titulo NVARCHAR(200) NOT NULL,
-    artista NVARCHAR(150),
-    descripcion NVARCHAR(MAX),
-    categoria NVARCHAR(100),
+    titulo VARCHAR(200) NOT NULL,
+    artista VARCHAR(150),
+    descripcion VARCHAR(MAX),
+    categoria VARCHAR(100),
     fecha_creacion DATE,
-    imagen_url NVARCHAR(300),
+    imagen_url VARCHAR(300),
     disponible BIT DEFAULT 1
+
 );
