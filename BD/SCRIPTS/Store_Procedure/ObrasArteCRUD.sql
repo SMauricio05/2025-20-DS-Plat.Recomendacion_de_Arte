99c4bf6665b1b5aabd11caa6b677075554bff8be
SMauricio05 <72190212@continental.edu.pe>
2025-10-23 18:12:06 -0500

    Recreate sp_InsertarObra and sp_ListarObras procedures
    Recreated stored procedures for inserting and listing artworks, ensuring correct syntax and functionality.

--- BD/SCRIPTS/Store_Procedure/ObrasArteCRUD.sql
+++ BD/SCRIPTS/Store_Procedure/ObrasArteCRUD.sql
@@ -15,19 +15,20 @@ GO
 CREATE PROCEDURE sp_ListarObras
 AS
 BEGIN
-    SELECT * FROM ObrasArte WHERE disponible = 1;
+    SELECT * FROM ObrasArte WHERE disponibilidad = 1;
 END;
 GO
 
 --INSERTAMOS OBRA
 
 EXEC sp_InsertarObra
-    @titulo = 'Paisaje Cultural de Junín',
+    @titulo = 'Paisaje Cultural de JunÃ­n',
     @artista = 'Angelo Rojas',
-    @descripcion = 'Obra que representa los paisajes naturales de la región Junín.',
-    @categoria = 'Fotografía',
+    @descripcion = 'Obra que representa los paisajes naturales de la regiÃ³n JunÃ­n.',
+    @categoria = 'FotografÃ­a',
     @fecha_creacion = '2024-08-12',
     @imagen_url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeZlnnmqrmSSwuPuCgeBSiokGSiBXk6blyzA&s';
+
 
 
 
