8e06bb72ca1820c581234809a53000e9480b6dad
SMauricio05 <72190212@continental.edu.pe>
2025-10-23 18:10:48 -0500

    Change tipo column type from NVARCHAR to VARCHAR

--- BD/SCRIPTS/Creación/tablaRecomendaciones.sql
+++ BD/SCRIPTS/Creación/tablaRecomendaciones.sql
@@ -3,6 +3,7 @@ CREATE TABLE Recomendaciones (
     id_usuario INT FOREIGN KEY REFERENCES Usuarios(id_usuario),
     id_obra INT NULL FOREIGN KEY REFERENCES ObrasArte(id_obra),
     id_evento INT NULL FOREIGN KEY REFERENCES Eventos(id_evento),
-    tipo NVARCHAR(20) CHECK (tipo IN ('OBRA', 'EVENTO')),
+    tipo VARCHAR(20) CHECK (tipo IN ('OBRA', 'EVENTO')),
     fecha_recomendacion DATETIME DEFAULT GETDATE()
+
 );
