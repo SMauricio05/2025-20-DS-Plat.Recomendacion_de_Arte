bc8677072fb5e99b9319962a35fb66f7f5a9e143
SMauricio05 <72190212@continental.edu.pe>
2025-10-30 00:52:34 -0500

    Add files via upload

---
+++ sys/MODELO/PRUEBA.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package artify;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class PRUEBA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CONEXION miConexion = new CONEXION();
+        miConexion.conectar();
+    }
+    
+}
