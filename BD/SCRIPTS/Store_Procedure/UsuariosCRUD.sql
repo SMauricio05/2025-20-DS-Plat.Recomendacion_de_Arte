80d6e39035a34ac420bb8331f3d8f3f14cddffec
Cristhian21-creator <75459622@continental.edu.pe>
2025-11-05 21:31:50 -0500

    UsuariosCRUD.sql

--- BD/SCRIPTS/Store_Procedure/UsuariosCRUD.sql
+++ BD/SCRIPTS/Store_Procedure/UsuariosCRUD.sql
@@ -32,4 +32,69 @@ BEGIN
     WHERE id_usuario = @id_usuario;
 END;
 GO
+
 EXEC sp_ListarUsuarios;
+
+
+-- Eliminar usuario (marcar como inactivo)
+CREATE PROCEDURE sp_EliminarUsuario
+    @id_usuario INT
+AS
+BEGIN
+    UPDATE Usuarios
+    SET activo = 0
+    WHERE id_usuario = @id_usuario;
+END;
+GO
+
+
+
+
+-- Inserción de 7 usuarios de ejemplo
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Juan Pérez', 
+    @email = 'juan.perez@gmail.com', 
+    @contrasena = 'juan123', 
+    @intereses = 'Arte, música, cine';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'María López', 
+    @email = 'maria.lopez@gmail.com', 
+    @contrasena = 'maria456', 
+    @intereses = 'Pintura, lectura, danza';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Carlos Gómez', 
+    @email = 'carlos.gomez@gmail.com', 
+    @contrasena = 'carlos789', 
+    @intereses = 'Escultura, teatro, fotografía';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Lucía Torres', 
+    @email = 'lucia.torres@gmail.com', 
+    @contrasena = 'lucia101', 
+    @intereses = 'Diseño, arte digital, viajes';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Pedro Ramírez', 
+    @email = 'pedro.ramirez@gmail.com', 
+    @contrasena = 'pedro202', 
+    @intereses = 'Cine, pintura, literatura';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Ana Castillo', 
+    @email = 'ana.castillo@gmail.com', 
+    @contrasena = 'ana303', 
+    @intereses = 'Música, fotografía, moda';
+
+EXEC sp_InsertarUsuario 
+    @nombre = 'Diego Fernández', 
+    @email = 'diego.fernandez@gmail.com', 
+    @contrasena = 'diego404', 
+    @intereses = 'Arquitectura, diseño gráfico, escultura';
+GO
+---PARA VERIFICAR LOS USUARIOS REGISTRADOS
+
+EXEC sp_ListarUsuarios;
+GO
